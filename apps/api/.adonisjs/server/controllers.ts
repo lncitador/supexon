5de3ad46872051a8b88ff195808ea870714d5878
walafffernandes <56723935-walafffernandes@users.noreply.replit.com>
2026-05-11 03:44:02 +0000

    Restored to 'd88e2d23339bdaf534cdf1d15ae815c2b6e94ae5'
    Replit-Restored-To: d88e2d23339bdaf534cdf1d15ae815c2b6e94ae5

--- apps/api/.adonisjs/server/controllers.ts
+++ apps/api/.adonisjs/server/controllers.ts
@@ -3,4 +3,8 @@
  * DO NOT EDIT manually
  */
 
-export const controllers = {}
+export const controllers = {
+  AccessTokens: () => import('#controllers/access_tokens_controller'),
+  NewAccount: () => import('#controllers/new_account_controller'),
+  Profile: () => import('#controllers/profile_controller'),
+}
